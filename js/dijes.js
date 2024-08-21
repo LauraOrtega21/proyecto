88414e8144f6f543db2847614a7ac1d7de1e2cba
JoseJulianSilvaEstupinanADSO <julianelgalann@gmail.com>
2024-08-21 06:51:49 -0500

    arreglo anillo

---
+++ js/dijes.js
@@ -0,0 +1,102 @@
+const dijes = [
+    {
+        imagen: "../Imagenes/Oro1.jpg",
+        nombre: "Anillo de Oro ",
+        valor: "$1.200.000",
+        material: "oro"
+    },
+    {
+        imagen: "../Imagenes/Plata1.jpg",
+        nombre: "Anillo de Plata ",
+        valor: "$1.100.000",
+        material: "plata"
+    },
+    {
+        imagen: "../Imagenes/Bronce1.jpg",
+        nombre: "Anillo de Bronce ",
+        valor: "$1.000.000",
+        material: "bronce"
+    },
+    {
+        imagen: "../Imagenes/Acero1.jpg",
+        nombre: "Anillo de Acero ",
+        valor: "$1.300.000",
+        material: "acero"
+    },
+    {
+        imagen: "../Imagenes/Oro2.jpg",
+        nombre: "Anillo de Oro ",
+        valor: "$1.200.000",
+        material: "oro"
+    },
+    {
+        imagen: "../Imagenes/Plata2.jpg",
+        nombre: "Anillo de Plata ",
+        valor: "$1.100.000",
+        material: "plata"
+    },
+    {
+        imagen: "../Imagenes/Bronce2.jpg",
+        nombre: "Anillo de Bronce ",
+        valor: "$1.000.000",
+        material: "bronce"
+    },
+    {
+        imagen: "../Imagenes/Acero2.jpg",
+        nombre: "Anillo de Acero ",
+        valor: "$1.300.000",
+        material: "acero"
+    },
+    {
+        imagen: "../Imagenes/Oro3.jpg",
+        nombre: "Anillo de Oro",
+        valor: "$1.200.000",
+        material: "oro"
+    },
+    {
+        imagen: "../Imagenes/Plata3.jpg",
+        nombre: "Anillo de Plata",
+        valor: "$1.100.000",
+        material: "plata"
+    },
+    {
+        imagen: "../Imagenes/Bronce3.jpg",
+        nombre: "Anillo de Bronce",
+        valor: "$1.000.000",
+        material: "bronce"
+    },
+    {
+        imagen: "../Imagenes/Acero3.jpg",
+        nombre: "Anillo de Acero",
+        valor: "$1.300.000",
+        material: "acero"
+    }
+];
+
+function catalogoDijes(filteredAnillos) {
+    const catalog = document.getElementById('catalog');
+    catalog.innerHTML = '';
+    filteredAnillos.forEach(dije => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = `
+            <img src="${dije.imagen}" alt="${dije.nombre}">
+            <h3>${dije.nombre}</h3>
+            <p>${dije.valor}</p>
+            <button onclick="verMas('${dije.nombre}')">Ver más</button>
+        `;
+        catalog.appendChild(card);
+    });
+}
+
+function filterCatalog() {
+    const material = document.getElementById('materialFilter').value;
+    const filteredAnillos = material === 'all' ? dijes : dijes.filter(anillo => anillo.material === material);
+    catalogoDijes(filteredAnillos);
+}
+
+function verMas(nombre) {
+    alert(`Más detalles del ${nombre}`);
+}
+
+catalogoDijes(dijes);
